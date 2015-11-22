86f774aa13a677e7e42e3bd9ffc10e9cd366a8b3
baiyangx <350916161@qq.com>
2015-11-22 12:56:18 +0800

    Initial Commit

---
+++ EOJ1240/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  EOJ1240
+//
+//  Created by frank on 15-11-22.
+//  Copyright (c) 2015年 frank. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
